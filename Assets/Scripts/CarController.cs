850bcb45aff1d08750a43883097ac6e37091ca1d
cbleck <carlos@capptu.com>
2016-06-18 14:14:56 -0500

    Lab7

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarController : MonoBehaviour {
+
+    public float speed;
+    private float startTime;
+    private bool align, moving;
+    private float normalizator;
+    private Vector3 startPosition;
+    private Vector3 destPosition;
+    private Vector3 startForward;
+    private Vector3 endForward;
+    private float rotationSpeed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (align) {
+            transform.LookAt(Vector3.Lerp(startForward, endForward, (Time.time - startTime) * rotationSpeed));
+            if ((Time.time - startTime) >= (1 / rotationSpeed)){
+                align = false;
+                startTime = Time.time;
+            }
+        }
+        else if (moving)
+        {
+            transform.position = Vector3.Lerp(startPosition, destPosition, (Time.time - startTime) / (normalizator * (1 / speed)));
+
+            if ((Time.time - startTime) >= (normalizator * (1 / speed)))
+                moving = false;
+        }
+
+	
+	}
+
+    public void move(Vector3 endPosition) {
+
+        startTime = Time.time;
+        destPosition = endPosition;
+        startPosition = transform.position;
+        normalizator = Vector3.Magnitude(destPosition-startPosition);
+        moving = true;
+        align = true;
+        startForward = transform.forward;
+        startForward.y = 0;
+        endForward = destPosition;
+        endForward.y = 0;
+
+        rotationSpeed = 10;
+    }
+}
---
+++ Assets/Scripts/CarSelector.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public enum SelectorState {
+    CAR_SELECTION,
+    TERRAIN_SELECTION
+}
+
+public class CarSelector : MonoBehaviour {
+
+    public SelectorState currentState;
+    private GameObject tmpCar;
+    private Vector3 destPosition;
+    private float velocity;
+
+    private Ray mouseRay;
+    private RaycastHit raycastHit;
+
+	// Use this for initialization
+	void Start () {
+
+        currentState = SelectorState.CAR_SELECTION;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetMouseButtonDown(0)) {
+
+            mouseRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(mouseRay, out raycastHit)) {
+
+                if (raycastHit.transform.tag == "Car")
+                    tmpCar = raycastHit.transform.gameObject;
+                //else tmpCar = null;
+
+                switch (currentState) {
+                    case SelectorState.CAR_SELECTION:
+                        if (tmpCar != null)
+                            currentState = SelectorState.TERRAIN_SELECTION;
+                        break;
+                    case SelectorState.TERRAIN_SELECTION:
+                        /*
+                        if (tmpCar != null)
+                            currentState = SelectorState.CAR_SELECTION;
+                        else {*/
+                        if (raycastHit.transform.tag != "Car"){
+                            destPosition = raycastHit.point;
+                            tmpCar.SendMessage("move", destPosition, SendMessageOptions.DontRequireReceiver);
+                        }
+                        //}
+                        break;
+                }//End switch current state
+            }//Endraycast
+        }
+	}
+}
